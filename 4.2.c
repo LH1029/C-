95ab4404066e3028e1bb91f294de521389c2c124
LH1029 <83651954+LH1029@users.noreply.github.com>
2021-05-04 21:38:49 +0800

    Add files via upload

---
+++ 4.2.c
@@ -0,0 +1,50 @@
+//#include <stdio.h> 
+//
+//int Max(int x,int y){
+//
+//	if(x>y)
+//		return x;
+//	else 
+//		return y;
+//}
+//
+//int main(){
+//	int a=0, b=0;
+//
+//	scanf("%d,%d",&a,&b);
+//	printf("%d", Max(a,b));
+//
+//}
+
+//一.求两个数的较大值
+//	int M = 0;
+//	scanf("%d", &M);
+//	if (1 >= M&&M >= 100000)
+//	{
+//		return 0;
+//	}
+//	if (M % 5 == 0)
+//	{
+//		printf("Yes\n");
+//	}
+//	else
+//	{
+//		printf("No\n");
+//	}
+//}
+//
+//int main() 
+//{
+//	int i = 0;
+//	scanf("%d", &i);
+//	for(i; i<=100; i++)
+//	{
+//		
+//		if (1 == i % 2)
+//		{
+//			printf("%d\n", i);
+//		}
+//	}
+//	return 0;
+//
+//
---
+++ 4.6.c
@@ -0,0 +1,50 @@
+//#include <stdio.h>
+//
+//int main()
+//{
+	//一.小飞机
+	//printf("   **   \n");
+	//printf("   **   \n");
+	//printf("************\n");
+	//printf("************\n");
+	//printf("  * *  \n");
+	//printf("  * *  \n");
+
+
+	//#include <stdio.h>
+	//
+	//int main()
+	//{
+	//	for (int i = 0; i<6; i++)
+	//	{
+	//		switch (i)
+	//		{
+	//		case 1:
+	//		case 2:printf("     **     \n"); break;
+	//		case 3:
+	//		case 4:printf("************\n"); break;
+	//		case 5:
+	//		case 6:printf("    *  *    \n"); break;
+	//		}
+	//	}
+	//
+	//}
+
+	//二.被5整除问题
+//	int M = 0;
+//	scanf("%d", &M);
+//	if (100000 <= M&&M <= 1)
+//	{
+//		return 0;
+//	}
+//	if (M % 5 == 0)
+//	{
+//		printf("YES\n");
+//	}
+//	else
+//	{
+//		printf("NO\n");
+//	}
+//
+//	return 0;
+//}
